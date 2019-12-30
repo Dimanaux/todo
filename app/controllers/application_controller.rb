ee9607287145c8080218a16387ea9f6867dc96f5
Dmitry Barskov <barskoidim@gmail.com>
2019-12-30 22:55:46 +0300

    add authentication of requests to application controller by default, provide current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
+# frozen_string_literal: true
+
+# Base controller
 class ApplicationController < ActionController::API
+  before_action :authenticate_request
+  attr_reader :current_user
+
+  private
+
+  def authenticate_request
+    result = Requests::Authenticate.call(jwt: request.headers['Authorization'])
+    if result.success?
+      @current_user = result.user
+    else
+      render(json: result.to_h.slice(:error), status: 401)
+    end
+  end
 end
