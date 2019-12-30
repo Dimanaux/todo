90920611b591b19786ef47d0cfc41a863c2e4b38
Dmitry Barskov <barskoidim@gmail.com>
2019-12-30 22:56:47 +0300

    set up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    resources :users, only: [:create]
+    get '/users/my', to: 'users#show'
+    resources :authentication, only: [:create]
+  end
 end
