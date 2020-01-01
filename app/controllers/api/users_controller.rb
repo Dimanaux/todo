b9c141b52f8946729d756550aa5754feea996f8d
Dmitry Barskov <barskoidim@gmail.com>
2020-01-01 18:15:22 +0300

    refactor users controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -6,23 +6,25 @@ module Api
     skip_before_action :authenticate_request, only: [:create]
 
     def show
-      render(json: { id: current_user.id, email: current_user.email })
+      render json: current_user
     end
 
     def create
       result = Users::Create.call(params)
 
       if result.success?
-        user = result.user
-        render(json: { id: user.id, email: user.email }, status: :created)
+        render json: result.user, status: :created
       else
-        render(json: result.to_h.slice(:error), status: :unprocessable_entity)
+        render json: result.to_h.slice(:error), status: :unprocessable_entity
       end
     end
 
     def destroy
-      # TODO: delete one's account
-      @user.destroy
+      # TODO: think: client still can store token after
+      # account deletion and is recognized, so it can
+      # get internal errors (500) instead of unauthorized (401)
+      current_user.reload
+      current_user.destroy
     end
 
     private
