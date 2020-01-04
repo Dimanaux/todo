f5b1db7c205e1426253e360f64fa3d99cc8d67bb
Dmitry Barskov <barskoidim@gmail.com>
2020-01-04 20:03:27 +0300

    Describe routing

--- spec/routing/todo_items_routing_spec.rb
+++ spec/routing/todo_items_routing_spec.rb
@@ -2,30 +2,30 @@
 
 require 'rails_helper'
 
-describe Api::TodoItemsController, type: :routing, skip: true do
+describe Api::TodoItemsController, type: :routing do
   describe 'routing' do
     it 'routes to #index' do
-      expect(get: '/todo_items').to route_to('todo_items#index')
+      expect(get: '/api/todo_lists/1/todo_items').to route_to('api/todo_items#index', todo_list_id: '1')
     end
 
     it 'routes to #show' do
-      expect(get: '/todo_items/1').to route_to('todo_items#show', id: '1')
+      expect(get: 'api/todo_items/1').to route_to('api/todo_items#show', id: '1')
     end
 
     it 'routes to #create' do
-      expect(post: '/todo_items').to route_to('todo_items#create')
+      expect(post: '/api/todo_lists/1/todo_items').to route_to('api/todo_items#create', todo_list_id: '1')
     end
 
     it 'routes to #update via PUT' do
-      expect(put: '/todo_items/1').to route_to('todo_items#update', id: '1')
+      expect(put: '/api/todo_items/1').to route_to('api/todo_items#update', id: '1')
     end
 
     it 'routes to #update via PATCH' do
-      expect(patch: '/todo_items/1').to route_to('todo_items#update', id: '1')
+      expect(patch: '/api/todo_items/1').to route_to('api/todo_items#update', id: '1')
     end
 
     it 'routes to #destroy' do
-      expect(delete: '/todo_items/1').to route_to('todo_items#destroy', id: '1')
+      expect(delete: '/api/todo_items/1').to route_to('api/todo_items#destroy', id: '1')
     end
   end
 end
